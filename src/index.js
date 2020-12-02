e774570bf5b083b3d2e61561f9d722268cc8951c
Stephan <jimno345@gmail.com>
2020-12-02 16:37:21 +0100

    Add github workflow.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-console.log('generator goes gchchchchchchchchhch')
+import yargs from 'yargs'
+
+const args = yargs.argv
+
+console.log('generator goes gchchchchchchchchhch', args)
